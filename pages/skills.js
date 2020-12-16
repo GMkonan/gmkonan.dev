d045cf587af0b7800d23341a6464ed949d42005a
GMkonan <guilhemont@hotmail.com>
2020-12-15 22:01:19 -0200

    added small changes to div in skills,deciding in using or not

--- pages/skills.js
+++ pages/skills.js
@@ -2,6 +2,20 @@ import {DiPython, DiGit, DiTerminal, DiJavascript1, DiReact} from 'react-icons/d
 import {Box, makeStyles, Typography, Divider} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
+    div: {
+        width: "auto",
+        height: "1000px",
+        maxHeight:"auto",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundSize: "cover",
+        [theme.breakpoints.down('md')] : {
+            height:"auto",
+            
+        }
+  },    
     skills: {
         display:"flex", 
         alignItems:"center",
@@ -27,7 +41,7 @@ const Skills = () => {
 
 
     return(
-        <Box>
+        <Box color="white" bgcolor="#1b1c1f" className={classes.div} id="skills">
         <Typography style={{justifyContent:"center"}} variant="h3">Skill Set</Typography>
         <Divider className={classes.dividerColor} />
         <Box className={classes.skills}>
