f63b29454f199f3663f5edb94015ac82e3290890
GMkonan <guilhemont@hotmail.com>
2020-12-25 02:21:47 -0300

    updated the contact part

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import {Box, Typography, makeStyles, IconButton} from '@material-ui/core';
-import {IoIosMail} from 'react-icons/io';
-import {SiReddit, SiTwitter, SiLinkedin, SiDevDotTo, SiGithub} from 'react-icons/si';
+import {Box, Typography, makeStyles, IconButton, } from '@material-ui/core';
+import twitter from './../images/icons/twitter.png';
+import email from './../images/icons/email.png';
+import reddit from './../images/icons/reddit.png';
+import github from './../images/icons/github.png';
+import linkedin from './../images/icons/linkedin.png';
 
 const Contact = () => {
 
@@ -15,37 +18,38 @@ const Contact = () => {
             backgroundSize: "cover",
             flexDirection: "column",
             backgroundPosition: "center top"
+        },
+        sayHi: {
+            fontFamily:"Courier New",
+            letterSpacing: "3px",
+            paddingBottom: "0.8rem"
         }
     });
 
     const classes = useStyles();
     
     return (
-        <Box width={1} color="white" className={classes.background} id="contact" bgcolor="#434745">
-            <Typography variant="h4" style={{ fontFamily:"Fantasy", letterSpacing:"3px" }}>CONTACT ME</Typography>
+        <Box width={1} color="white" className={classes.background} id="contact" bgcolor="#2f3133">
+            <Typography variant="h5" className={classes.sayHi}> <b>Social Medias</b> </Typography>
                 <Box>
                     <IconButton onClick={() => window.open('https://github.com/GMkonan')}>
-                        <SiGithub color="black" size="2.5rem" />
+                        <img src={github} height="40px" />
                     </IconButton>
 
                     <IconButton onClick={() => window.open('mailto:guilhemont@hotmail.com')}>
-                        <IoIosMail color="black" size="3rem" />
+                        <img src={email} height="40px" />
                     </IconButton>
 
                     <IconButton onClick={() => window.open('https://www.reddit.com/user/KonanGM')}>
-                        <SiReddit color="black" size="2.5rem" />
+                        <img src={reddit} height="40px" />
                     </IconButton>
 
                     <IconButton onClick={() => window.open('https://twitter.com/GuilhermeKonan')}>
-                        <SiTwitter color="black" size="2.5rem" />
+                        <img src={twitter} height="40px" />
                     </IconButton>
 
                     <IconButton onClick={() => window.open('https://www.linkedin.com/in/gmkonan/')}>
-                        <SiLinkedin color="black" size="2.5rem" />
-                    </IconButton>
-
-                    <IconButton onClick={() => window.open('https://dev.to/gmkonan')}>
-                        <SiDevDotTo color="black" size="2.5rem" />
+                        <img src={linkedin} height="40px" />
                     </IconButton>
                 </Box>
         </Box>
