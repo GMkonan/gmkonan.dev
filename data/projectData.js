8836290a17736b646e226c1f291746b229d45102
GMkonan <guilhemont@hotmail.com>
2021-03-02 03:20:52 -0300

    added 2 projects

--- data/projectData.js
+++ data/projectData.js
@@ -2,6 +2,8 @@ import websiteImage from "./../images/website.png";
 import blog from "./../images/blog.png";
 import anime from "./../images/anime.jpg";
 import githubImage from "./../images/github.png";
+import api from "../images/api.png";
+import moveit from "../images/moveit.png";
 
 const projects = [
     {
@@ -23,6 +25,24 @@ const projects = [
         url2: "https://my-blog-vert.vercel.app/",
         buttonText2:"Website",
         tech: "React, styledComponents, NextJS"
+    },
+    {
+        title: "MoveIt",
+        image: moveit,
+        titleCard: "MoveIt Website",
+        text: "A website made to use the pomodoro technique with a game felling, gain xp and level up!",
+        url: "https://github.com/GMkonan/MoveIt",
+        url2: "https://moveit-alpha-lovat.vercel.app",
+        buttonText2:"Website",
+        tech: " Typescript, React, NextJS"
+    },
+    {
+        title: "NPS API",
+        image: api,
+        titleCard: "NPS API",
+        text: "A backend application made to calculate a company's NPS. (Net Promoter Score)",
+        url: "https://github.com/GMkonan/RotaNodeJSAPI",
+        tech: "Typescript, Express, Typeorm, sqlite3"
     },
     {
         title: "Github Automation",
