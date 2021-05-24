bf2eddc86718dc70ac47457a95bf29fa819ee5ec
GMkonan <guilhemont@hotmail.com>
2021-05-24 16:54:28 -0300

    first commit to refactore dev branch

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Button, 
 Box, 
-makeStyles,
 Typography,
 AppBar,
 Toolbar,
@@ -13,42 +12,8 @@ Divider,
 Link
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import useStyles from './test';
 const NavBar = () => {
-
-    const useStyles = makeStyles((theme) => ({
-        hideButtons: {
-            [theme.breakpoints.down('xs')]:{
-                display:"none"
-            }
-          },
-          hideIconMenu: {
-            [theme.breakpoints.up('sm')]:{
-                display: "none"
-            }
-          },
-        button:{
-            background: "inherit",
-            fontSize: "0.9rem",
-            height: 40,
-            padding: "0 1rem",
-            color: "black",
-        },
-        title: {
-          color: "Black"
-        },
-        link: {
-            textDecoration: 'none',
-            flex: 1,
-            '&:hover':{
-              cursor: 'pointer',
-              textDecoration: "none",
-            }
-        },
-        appbar: {
-            background: "#ffffff"
-        }
-    }));
     
     const scrollTo = (id) => {
         const element = document.getElementById(id);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../theme';
+
 
 export default function MyApp(props) {
   const { Component, pageProps } = props;
-
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
 
   return (
     <React.Fragment>
@@ -28,16 +18,9 @@ export default function MyApp(props) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
-      <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
-      </ThemeProvider>
     </React.Fragment>
   );
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
