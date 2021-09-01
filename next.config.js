f73f5b4852b58cec4cbc3fffa3cf6a3ab5fddca0
GMkonan <guilhemont@hotmail.com>
2021-09-01 11:56:33 -0300

    setup next

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
-// next.config.js
-const withImages = require('next-images')
-module.exports = withImages({  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
-    }
-
-    return config
-  }})
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+}
