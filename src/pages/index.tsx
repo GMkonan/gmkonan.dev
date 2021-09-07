afc838565998c5d0b9a143c7b4259d0f5699601f
GMkonan <guilhemont@hotmail.com>
2021-09-07 00:54:21 -0300

    finish twin setup, add front page with polygon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,41 @@
 import type { NextPage } from 'next'
-import 'twin.macro';
+import tw, { css } from 'twin.macro';
+
+const Title = tw.h1`text-white text-4xl mt-20 ml-20 whitespace-pre-line leading-loose font-bold tracking-widest text-green-500`;
 
 const Home: NextPage = () => {
   return (
-    <div tw="flex w-full h-screen items-center justify-center">
-      <h1>Home page GMkonan.dev</h1>
+    <>
+    <style global jsx>{`
+    html,
+    body,
+    #__next {
+      height: 100vh;
+      overflow: hidden;
+      margin: 0;
+      padding: 0;
+      font-family: Inter;
+      background-color: #f5f5f5;
+    }
+    `}</style>
+    <div tw="flex h-full w-full relative">
+      <div tw="bg-white shadow-xl flex w-full h-16 fixed z-20">
+        {/* NavBar */}
+      </div>
+      <div tw="flex items-center" css={[`
+      	background-color: #1D1F21;
+        width: 100%;
+        clip-path: polygon(0 42%, 53% 0, 100% 0, 100% 35%, 25% 100%, 0 100%);
+        position: relative;
+      `]}>
+        <Title>
+          I’m Konan, a Creative {"\n"}
+          {'&'} Self-learning {"\n"}
+          Developer
+        </Title>
+      </div>
     </div>
+    </>
   )
 }
 
