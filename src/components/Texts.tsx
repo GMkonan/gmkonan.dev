b5a154598d10ac6445d48e0080ae40e180247ef0
GMkonan <guilhemont@hotmail.com>
2021-09-11 20:19:00 -0300

    Texts component

---
+++ src/components/Texts.tsx
@@ -0,0 +1,5 @@
+import tw from 'twin.macro';
+
+export const TextSm = tw.div`text-white text-2xl ml-20 font-light tracking-widest leading-tight text-black`;
+export const TextMd = tw.div`text-white text-4xl ml-28 font-medium tracking-widest leading-tight text-black`;
+export const TextXl = tw.div`text-white text-6xl ml-24 font-bold tracking-widest leading-tight text-black`;
