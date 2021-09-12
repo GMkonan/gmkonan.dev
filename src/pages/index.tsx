a78fcc87488d2a3b60f01b3a8ac29ce0d49bde94
GMkonan <guilhemont@hotmail.com>
2021-09-11 21:43:00 -0300

    [Home] add social media svgs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
+import Image from 'next/image';
 import tw, { css } from 'twin.macro';
 import { TextSm, TextMd, TextXl } from '@components/Texts';
 import NavBar from '@components/NavBar';
+import GithubLogo from '@images/github-logo.svg';
+import LinkedinLogo from '@images/linkedin-logo.svg';
+import TwitterLogo from '@images/twitter-logo.svg';
 
 const Home: NextPage = () => {
   return (
@@ -23,7 +27,7 @@ const Home: NextPage = () => {
       <div tw="flex w-full h-16 m-4">
         <NavBar />
       </div>
-      <div tw="flex flex-col justify-center mt-16">
+      <div tw="flex flex-col justify-center mt-16 w-1/2">
         <TextSm>
           Hello, I’m
         </TextSm>
@@ -33,11 +37,18 @@ const Home: NextPage = () => {
         <TextMd>
           Full-Stack Developer
         </TextMd>
+        <div tw="flex items-center justify-evenly ml-32 mt-4 w-1/3">
+          <Image src={GithubLogo} alt="My Github" />
+          <Image src={LinkedinLogo} alt="My Github" />
+          <Image src={TwitterLogo} alt="My Github" />
+        </div>
       </div> 
     </div>
     <div tw="flex flex-col justify-center items-center w-full h-full"> 
-      <div tw="flex items-center justify-center w-96 h-96 shadow-xl rounded-xl mb-28 text-brand-color">
+      <div tw="flex w-96 h-96 shadow-xl rounded-xl mb-48 text-brand-color">
+        <div tw="mt-4 ml-4">
           Class Developer
+        </div>
       </div>
     </div>
     </>
