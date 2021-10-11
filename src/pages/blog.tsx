aedd07ff77d3ee2a0b326c3cc67660c726e2086f
GMkonan <guilhemont@hotmail.com>
2021-10-11 00:16:33 -0300

    Completed card code component

---
+++ src/pages/blog.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+
+
+const Blog: NextPage = () => {
+
+    return(
+        <div>
+            <article>
+                stuff
+            </article>
+        </div>
+    );
+}
+
+export default Blog;
