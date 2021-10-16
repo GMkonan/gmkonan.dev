27a9da287ed58513547f8b4209e31c5b75667695
GMkonan <guilhemont@hotmail.com>
2021-10-16 13:50:26 -0300

    Update navbar/structure blog page

--- src/common/components/NavBar.tsx
+++ src/common/components/NavBar.tsx
@@ -2,15 +2,15 @@ import tw from 'twin.macro';
 
 const NavBar = () => {
     
-    const Item = tw.button`ml-8 text-xl text-brand-color font-inter font-semibold bg-transparent border-none cursor-pointer`;
+    const Item = tw.a`ml-8 text-xl no-underline hover:underline text-brand-color font-inter font-semibold bg-transparent border-none cursor-pointer`;
 
     // Depending on your ideas this buttons will move user to other page or scroll to a specific part of the page (as SPA)
     // if you choose the scroll option take a look at a react lib (there is one only for this scroll effect)
 
     return (
-      <div tw="flex w-full h-16 m-4">
+      <div tw="flex sticky w-full h-16 m-4">
             <Item>Projects</Item>
-            <Item>Blog</Item>  
+            <Item href="/blog">Blog</Item>  
       </div>
     );
 }
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,32 @@
+import tw, { css } from 'twin.macro';
 import { NextPage } from "next";
+import Layout from "src/common/layouts/layout";
 
 
 const Blog: NextPage = () => {
 
     return(
-        <div>
-            <article>
-                stuff
-            </article>
-        </div>
+        <Layout>
+            <div tw="flex flex-col justify-center" css={[`width: 800px; margin: 0 auto;`]}>
+                <section css={['width: fit-content; margin-top: 150px;']}>
+                    <h1>Title</h1>
+                </section>
+                <section tw="flex">
+                    <article css={['width: 400px; height: 250px;']}>
+                        stuff
+                    </article>
+                    <article css={['width: 400px; height: 250px;']}>
+                        stuff
+                    </article>
+                    <article css={['width: 400px; height: 250px;']}>
+                        stuff
+                    </article>
+                    <article css={['width: 400px; height: 250px;']}>
+                        stuff
+                    </article>
+                </section>
+            </div>
+        </Layout>
     );
 }
 
