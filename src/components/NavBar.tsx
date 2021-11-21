a6800166ac8c7a817ff2e93e7e04d28349e47b17
GMkonan <guilhemont@hotmail.com>
2021-11-21 09:36:34 -0300

    Add NavBar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import MaxWidthWrapper from './MaxWidthWrapper';
+
+const NavBar = () => {
+    return(
+        <MaxWidthWrapper>
+            <Container>
+                <Logo>GMkonan</Logo>
+                <Links>
+                    <Link>My Work</Link>
+                    <Link>Projects</Link>
+                    <Link href="/blog">Articles</Link>
+                </Links>
+            </Container>
+        </MaxWidthWrapper>
+    );
+}
+
+const Container = styled.div`
+padding-top: 32px;
+display: flex;
+`
+
+const Logo = styled.h1`
+flex: auto;
+color: white;
+`
+
+const Links = styled.div`
+display: flex;
+color: white;
+gap: 20px;
+`
+
+const Link = styled.a`
+font-size: 1.3rem;
+font-weight: 500;
+color: inherit;
+text-decoration: inherit;
+cursor: pointer;
+`
+
+export default NavBar;
