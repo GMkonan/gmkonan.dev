0d3a7348a943d5005195b6d9b8bea6b987a651ec
GMkonan <guilhemont@hotmail.com>
2021-12-23 22:38:37 -0300

    Add slug by post and mdx rendering

--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -3,17 +3,35 @@ import { MDXRemote } from 'next-mdx-remote'
 import MaxWidthWrapper from '../../components/MaxWidthWrapper'
 import fs from 'fs'
 import matter from 'gray-matter';
+import styled from 'styled-components';
+import prism from "remark-prism";
 
 export default function TestPage({ source }: any) {
 
+  const components = { h2: (props:any) => <H2 variant="h2" {...props} /> }
+
   return (
     <MaxWidthWrapper>
-      <MDXRemote {...source} />
+      <ArticleContainer>
+        <MDXRemote {...source} components={components} />
+      </ArticleContainer>
     </MaxWidthWrapper>
   )
 }
 
+const ArticleContainer = styled.div`
+  //margin: 56px 0;
+  background: #fff;
+  border-radius: 32px;
+  padding: 20px;
+  //font-size: 1.1rem;
+  line-height: 26px;
+`
 
+const H2 = styled.h2`
+  margin: 20px 0;
+  color: #232946;
+`
 
 export async function getStaticPaths() {
     return {
@@ -26,6 +44,6 @@ export async function getStaticProps({params}: any) {
   const post = fs.readFileSync(`./posts/${params.slug}.mdx`)
   const article:any = matter(post) 
 
-  const mdxSource = await serialize(article.content)
+  const mdxSource = await serialize(article.content, {mdxOptions: {remarkPlugins: [prism]}})
   return { props: { source: mdxSource } }
 }
