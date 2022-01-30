f25f4a1b484f5efebf4ed3fb1eb47ee3ff70586b
GMkonan <guilhemont@hotmail.com>
2022-01-30 15:56:58 -0300

    Fix color theme type

--- src/themes.ts
+++ src/themes.ts
@@ -10,6 +10,7 @@ export const darkTheme = {
     primary50: 'hsl(205, 84%, 30%)',
     green: '#84cc16',
     black: "#ffffff",
+    blue50Blurred: 'hsl(205, 25%, 55%, 0.65)',
 }
 
 export const lightTheme = {
@@ -19,10 +20,10 @@ export const lightTheme = {
     gray: 'hsl(212, 13%, 43%)',
     blue400: '#3da9fc',
     blue50: 'hsl(205, 95%, 92%)',
-    blue50Blurred: 'hsl(205, 25%, 55%, 0.65)',
     red: '#ef4565',
     red500: 'hsl(353deg 100% 55%)',
     primary50: 'hsl(205, 84%, 30%)',
     green: '#84cc16',
     black: "#00000",
+    blue50Blurred: 'hsl(205, 25%, 55%, 0.65)',
 }
