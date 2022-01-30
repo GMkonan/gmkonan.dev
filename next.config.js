a7d03b627246913b8b1ad40b77efb4ea608e1b90
GMkonan <guilhemont@hotmail.com>
2022-01-30 03:22:31 -0300

    Add polyfill and process

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   reactStrictMode: true,
   webpack5: true,
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, "process": false };
+    config.resolve.fallback = { fs: false, "process": require.resolve("process/browser") };
     return config;
   },
 }
