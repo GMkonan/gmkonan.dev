a9f400e82cff3d02f3290c55bfb6538fb482b34c
GMkonan <guilhemont@hotmail.com>
2022-02-01 10:31:18 -0300

    Create svgs as components
    Small Fix: prevent theme not changing

--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -1,3 +1,4 @@
+import { Moon, Sun } from "@components/svgs"
 import useHasMounted from "src/hooks/useHasMounted"
 import { useTheme } from "src/store/Theme"
 import styled from "styled-components"
@@ -5,23 +6,19 @@ import styled from "styled-components"
 
 const ThemeToggle = () => {
 
+    const hasMounted = useHasMounted()
     const theme = useTheme(state => state.theme)
     const changeTheme = useTheme(state => state.ChangeTheme)
-    const hasMounted = useHasMounted()
 
     return (
         <Label>
             <Button 
-            onClick={(e) => changeTheme()}>
-            {hasMounted && theme == 'light' ? (
-                <svg width="22px" height="22px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" version="1.1" fill="none" stroke="var(--primary)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5">
-                <path d="m1.75 8c0 3.45 2.8 6.25 6.25 6.25 3.41-.0027 6.25-3 6.25-6-1 .5-4 1.5-6-.5s-1-5-.5-6c-3 0-6 2.84-6 6.25z"/>
-                </svg>
+            onClick={(e) => changeTheme()}
+            >
+            {theme == 'light' ? (
+                <Moon stroke="var(--primary)" width="22px" height="22px" />
             ) : (
-                <svg width="22px" height="22px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" version="1.1" fill="none" stroke="var(--primary)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5">
-                <circle cy="8" cx="8" r="3.25"/>
-                <path d="m2.75 13.25.5-.5m9.5 0 .5.5m-.5-10 .5-.5m-10 .5-.5-.5m-.50 5.25h-1m13.5 0h-1m-5.75 5.75v1m0-13.5v1"/>
-                </svg>
+                <Sun stroke="var(--primary)" width="22px" height="22px" />
             )}
             </Button>
         </Label>
@@ -29,6 +26,8 @@ const ThemeToggle = () => {
 }
 
 const Label = styled.label`
+    border: none;
+    cursor: pointer;
 `
 
 const Button = styled.button`
