1644a03ebb4adc80acfe9b9ef5f4cd6b307d29de
GMkonan <guilhemont@hotmail.com>
2022-02-06 13:53:02 -0300

    Start Updating hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,18 +2,41 @@ import { useRef } from 'react';
 import styled from 'styled-components';
 import { QUERIES } from '@constants';
 import MaxWidthWrapper from '@components/MaxWidthWrapper';
+import Image from 'next/image';
+import Me from "@public/mev2.png";
 
 const Hero = () => {
     return(
         <Container id="Hero">
             <MaxWidthWrapper>
+              <Wrapper>
+                <Presentation>
                 <Title>About Me</Title> {/* Later change to "My Work" */}
                 <SubTitle>Who Am I?</SubTitle>
                 <AboutMe>
+                  {/* Write this section better */}
                     My name is Guilherme Monteiro Pereira AKA Konan and I&apos;m a Self-Learning Software Developer.
                     I have always had been really curious guy, researching ideas that interest me and that got me into programming.
-                    I like making ideas come to reality with code.
+                    I like making ideas come to reality with code :).
+                    <br />
+                    <br />
+                    After nights and nights of study, today I work as a fullstack developer. 
+                    <br />
+                    You can check a few of the technologies I work with below:
+                    <ul>
+                      <li>Typescript</li>
+                      <li>React</li>
+                      <li>NextJS</li>
+                      <li>GraphQL</li>
+                    </ul>
                 </AboutMe>
+                </Presentation>
+
+                <ImageContainer>
+                  <Image src={Me} />
+                </ImageContainer>
+
+              </Wrapper>
             </MaxWidthWrapper>
         </Container>
     );
@@ -21,9 +44,9 @@ const Hero = () => {
 
 const Container = styled.div`
 display: flex;
-flex-direction: column;
+//flex-direction: column;
 height: 100%;
-background: var(--blue50); //#1a1e33
+background: var(--white); //#1a1e33
 transition: color 350ms ease 0s, background-color 350ms ease 0s;
 @media ${QUERIES.laptop} {
   
@@ -33,8 +56,21 @@ transition: color 350ms ease 0s, background-color 350ms ease 0s;
 }
 `
 
+const Wrapper = styled.div`
+  margin-top: 100px;
+  display: grid;
+  grid-template-columns: 60% 40%;
+  grid-template-rows: 1fr;
+  gap: 32px;
+  //flex-direction: column;
+`
+
+const Presentation = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
 const Title = styled.h1`
-margin-top: 225px;
 font-size: 5rem;
 color: var(--primary);
 
@@ -70,8 +106,9 @@ color: var(--primary);
 const AboutMe = styled.h2`
 padding: 0 0px;
 margin-top: 75px;
-font-size: 2rem;
-color: var(--primary);
+font-size: 1.3rem;
+line-height: 28px;
+color: var(--gray);
 @media ${QUERIES.laptop} {
 }
 @media ${QUERIES.tablet} {
@@ -80,5 +117,23 @@ color: var(--primary);
   font-size: 1.4rem;
 }
 `
+
+const ImageContainer = styled(Container)`
+  width: 350px;
+  height: 350px;
+  margin-left: 56px;
+  //border-radius: 100%;
+  //border: 3px var(--blue400) solid;
+  //animation: ${translateToLeft} 0.75s ease 0s 1 normal both running;
+  background: var(--blue50);
+
+  @media ${QUERIES.laptop} {
+  }
+  @media ${QUERIES.tablet} {
+  }
+  @media ${QUERIES.phone} {
+    display: none;
+  }
+`;
 
 export default Hero;
