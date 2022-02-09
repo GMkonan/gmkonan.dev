d41d3d44a21298cee54bb31fef4ddcb30db432b0
GMkonan <guilhemont@hotmail.com>
2022-02-09 06:44:29 -0300

    Remove zustand and related

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,26 +3,21 @@ import Head from 'next/head'
 import Footer from '@components/Footer';
 import GlobalStyles from '@components/GlobalStyles';
 import NavBar from '@components/NavBar';
-import { useTheme } from 'src/store/Theme';
 import { ThemeProvider } from 'next-themes'
-import { darkTheme, lightTheme } from 'src/themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-   //const theme = useTheme(state => state.theme)
-   //const colorTheme = theme == 'light' ? lightTheme : darkTheme
 
   return (
     <>
     <GlobalStyles />
-    <ThemeProvider>
-      <Head>
-        <title>GMkonan</title>
-      </Head>
-      <NavBar />
-      <Component {...pageProps} />
-      <Footer />
-    </ThemeProvider>
+      <ThemeProvider>
+        <Head>
+          <title>GMkonan</title>
+        </Head>
+        <NavBar />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
     </>
   );
 }
