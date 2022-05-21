33ebe72b9aa4faceed0581b68c23eebb6773bc6a
GMkonan <guilhemont@hotmail.com>
2022-05-21 12:30:17 -0300

    Change from property to name in twitter meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,15 +26,15 @@ const Home: NextPage = () => {
                     content="https://gmkonan.dev/website-image.png"
                 />
 
-                <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://gmkonan.dev" />
-                <meta property="twitter:title" content="GMKonan DEV" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:url" content="https://gmkonan.dev" />
+                <meta name="twitter:title" content="GMKonan DEV" />
                 <meta
-                    property="twitter:description"
+                    name="twitter:description"
                     content="Hello and welcome to my website :) you can check some info about me, what I do and what I know or go straight to my articles section!"
                 />
                 <meta
-                    property="twitter:image"
+                    name="twitter:image"
                     content="https://gmkonan.dev/website-image.png"
                 />
             </Head>
