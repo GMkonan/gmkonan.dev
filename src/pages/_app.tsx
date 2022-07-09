a23d278b59bc9c6e67783762e519e875541ff50e
GMkonan <guilhemont@hotmail.com>
2022-07-08 21:18:31 -0300

    Fix type error from react 18 compatibility

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,27 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Footer from '@components/Footer';
-import GlobalStyles from '@components/GlobalStyles';
-import NavBar from '@components/NavBar';
+import Footer from '@components/Footer'
+import GlobalStyles from '@components/GlobalStyles'
+import NavBar from '@components/NavBar'
 import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  return (
-    <>
-    <GlobalStyles />
-      <ThemeProvider>
-        <Head>
-          <title>GMkonan</title>
-        </Head>
-        <NavBar />
-        <Component {...pageProps} />
-        <Footer />
-      </ThemeProvider>
-    </>
-  );
+    // Fix until they fix this type issues in React 18
+    //https://github.com/styled-components/styled-components/issues/3738
+    const GlobalStyleProxy: any = GlobalStyles
+    return (
+        <>
+            <GlobalStyleProxy />
+            <ThemeProvider>
+                <Head>
+                    <title>GMkonan</title>
+                </Head>
+                <NavBar />
+                <Component {...pageProps} />
+                <Footer />
+            </ThemeProvider>
+        </>
+    )
 }
 
 export default MyApp
